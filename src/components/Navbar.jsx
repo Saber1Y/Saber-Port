976eae16d7410cceb904c4586600e43f049c42c7
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-25 16:31:15 +0100

    UPDATE: NAV ITEM CREATED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between' >
+      <nav>
+        <Link href='./'>Home</Link>
+        <Link href='./'>About</Link>
+        <Link href='./'>Projects</Link>
+      </nav>
+      <h2></h2>
+      <nav>
+        <Link href="" target={'_blank'}>X</Link>
+        <Link href="" target={'_blank'}>Linkedin</Link>
+        <Link href="" target={'_blank'}>Github</Link>
+      </nav>
+    </header>
   )
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import Head from 'next/head'
+import { Navbar } from '../index'
 
 
 const montserrat = Montserrat({
@@ -15,7 +16,8 @@ export default function App({ Component, pageProps }) {
        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
-    <main className={`${montserrat.variable} font-mont`}>
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <Navbar />
       <Component {...pageProps} />
     </main>
     </>
