942c429179f3e8bf21f8ec996682c460e7a9beac
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-25 16:04:53 +0100

    UPDATE: STLYES ADDED

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
+import Head from 'next/head'
 
 
 const montserrat = Montserrat({
@@ -9,9 +10,14 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+       <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
     <main className={`${montserrat.variable} font-mont`}>
-
       <Component {...pageProps} />
     </main>
+    </>
   ) 
 }
