233b76c9ed793fbd5127c0222db3a7c1beec8c6a
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 10:33:54 +0100

    DELETE: LAYOUT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Hero = ({ text, className }) => {
   return (
-    <div className={`${className} w-full h-full inline-block z-0 bg-light p-2`}>
+    <div className={`${className} w-full h-full inline-block z-0 bg-light p-3`}>
+        
       {text}
     </div>
   )
