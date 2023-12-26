fb112f7f700dc307f2320627bbcd104666fadec8
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 14:02:15 +0100

    CREATE: REACT TYPE ANIMAAATION

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Hero = ({ text, className }) => {
   return (
-    <div className={`${className} w-full h-full inline-block z-0 bg-light p-3`}>
-        
+    <div className={`${className} w-full h-full inline-block z-0 bg-light p-32`}>
       {text}
     </div>
   )
