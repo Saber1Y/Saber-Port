eb97ab2a64d0793c02a03fc3b01b6668e5a9e561
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 15:22:56 +0100

    UPDATE: ANIMATION ADDED

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,23 @@ import Image from 'next/image'
 import { Hero } from '../index'
 import Saber from '../../public/images/profile/Saber.png'
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
 
 
 export default function Home() {
+
+  const animate = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1, // Adjusted opacity value
+      transition: {
+        delay: 0.5, // Corrected property name
+      },
+    },
+  };
+
   return (
     <>
       <Head>
@@ -19,7 +33,7 @@ export default function Home() {
         <div className='w-full h-full inline-block z-0 bg-light p-8 lg:p-32 pt-0'>
           <div className='flex items-center justify-between  w-full'>
             <div className='w-full lg:w-2/3'>
-              <h2 className='text-3xl leading-7 tracking-tight lg:text-5xl'>Hello 👋🏾, I'm Saber</h2>
+              <motion.h2 className='text-3xl leading-7 tracking-tight lg:text-5xl' variants={animate} initial="initial" animate="animate">Hello 👋🏾, I'm Saber </motion.h2>
               <p className="font-extrabold opacity-60 text-3xl pt-3 leading-6 tracking-tighter font-mono no-underline sm:text-2xl md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
                 <TypeAnimation
                   sequence={[
@@ -36,12 +50,20 @@ export default function Home() {
                   repeat={Infinity}
                 />
               </p>
-              <p className='w-full text-2xl font-semibold opacity-80 '>
+              <p className='w-full text-2xl font-semibold opacity-80 py-2'>
                 I'm a highly motivated Full Stack Web3 Engineer with a deep passion for crafting innovative and efficient solutions. My expertise lies in building robust applications that leverage the latest web technologies, ensuring a seamless and interactive user experience. I thrive on challenges and am dedicated to staying at the forefront of emerging trends in the ever-evolving world of web development.
               </p>
+              <div>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Choose plan
+                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </button>
+              </div>
             </div>
-            <div className=' lg:block w-1/2 ml-8'> 
-              <Image src={Saber} alt='Saber' className='w-full h-full' />
+            <div className='lg:block w-1/2 ml-10' >
+              <Image src={Saber} alt='Saber' className='w-full h-full ml-10' />
             </div>
           </div>
         </div>
