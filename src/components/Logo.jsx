5a06f3e2874afd431c533c33cb72e9cbdf8e710e
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 16:30:43 +0100

    CREATE: REACT-ICONS

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -12,7 +12,7 @@ const Logo = () => {
       <MotionLink href="./" className='w-24 h-20 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold' whileHover={{
         backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
         transition: {duration:1, repeat: Infinity}
-      }}> <span className='p-4'>SABER</span> </MotionLink>
+      }}> <span className='p-4'>  SABER  </span> </MotionLink>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
           <div className='flex items-center justify-between  w-full'>
             <div className='w-full lg:w-2/3'>
               <motion.h2 className='text-3xl leading-7 tracking-tight lg:text-5xl' variants={animate} initial="initial" animate="animate">Hello 👋🏾, I'm Saber </motion.h2>
-              <p className="font-extrabold opacity-60 text-3xl pt-3 leading-6 tracking-tighter font-mono no-underline sm:text-2xl md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              <p className="font-extrabold  text-3xl pt-3 leading-6 tracking-tighter font-mono no-underline sm:text-2xl md:text-3xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
                 <TypeAnimation
                   sequence={[
                     'A Passionate Full Stack Web3 Engineer',
@@ -69,14 +69,14 @@ export default function Home() {
               <p className='w-full text-2xl font-semibold opacity-80 py-2'>
                 I'm a highly motivated Full Stack Web3 Engineer with a deep passion for crafting innovative and efficient solutions. My expertise lies in building robust applications that leverage the latest web technologies, ensuring a seamless and interactive user experience. I thrive on challenges and am dedicated to staying at the forefront of emerging trends in the ever-evolving world of web development.
               </p>
-              <div className='space-x-4'>
-                <Link type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="/resume.pdf" >
+              <div className='flex items-center space-x-4'>
+                <Link type="button" class="text-white bg-gradient-to-r to-emerald-600 from-sky-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="/resume.pdf" target={'_blank'} >
                   Resume
                   <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                   </svg>
                 </Link>
-                <Link type="button" class="px-5 py-2.5 text-sm font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="mailto:akintobiayodeji1@gmail.com" >
+                <Link type="button" class="text-white bg-gradient-to-r to-emerald-600 from-sky-400  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="mailto:akintobiayodeji1@gmail.com" target={'_blank'} >
                   <svg class="w-3.5 h-3.5 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
                     <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
                     <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
