a33a74b8f7cdb15c8ae19382e156b8e9da07f3a9
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 10:02:00 +0100

    CREATE: IMG FILE

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = ({ text, className }) => {
   return (
-    <div className={`${className} `}>
+    <div className={`${className} w-full h-full inline-block z-0 bg-light p-2`}>
       {text}
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import Logo from "./components/Logo";
 import CustomLink from "./components/CustomLink";
+import Hero from "./components/Hero";
 
 export {
     Navbar,
     Logo,
     CustomLink,
+    Hero
 }
