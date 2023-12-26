91c92ddb841a4258cb3e00a10d2fc55f3a98d39d
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-26 16:57:01 +0100

    CREATE: NAVBAR UPDATED

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 import { motion } from 'framer-motion'
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
 
 
 const MotionLink = motion(Link)
@@ -8,12 +10,23 @@ const MotionLink = motion(Link)
 
 const Logo = () => {
   return (
-    <div className='flex items-center justify-center mt-2'>
-      <MotionLink href="./" className='w-24 h-20 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold' whileHover={{
-        backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
-        transition: {duration:1, repeat: Infinity}
-      }}> <span className='p-4'>  SABER  </span> </MotionLink>
-    </div>
+<div className='flex items-center justify-center mt-2 space-x-4'>
+  <MotionLink
+    href="./"
+    className='w-32 h-18 bg-dark text-light flex items-center justify-center rounded-md text-2xl font-bold'
+    whileHover={{
+      backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+      transition: { duration: 1, repeat: Infinity }
+    }}
+  >
+    <span className='flex items-center'>
+      <FaAngleLeft />
+      SABER
+      <FaAngleRight />
+    </span>
+  </MotionLink>
+</div>
+
   )
 }
 
