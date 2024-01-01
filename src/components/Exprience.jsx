54fef1dd70d58c8308ee2c6672a1ecf57c675f06
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-01 18:00:02 +0100

    UPDATE: Exprience Updated

--- src/components/Exprience.jsx
+++ src/components/Exprience.jsx
@@ -3,13 +3,38 @@ import Details from './details'
 
 const Exprience = () => {
   return (
-    <div className='my-64 text-center'>
-      <h1 className='text-4xl text-dark font-bold capitalize mb-32 lg:text-7xl'>Exprience</h1>
+    <div className='my-32 text-center'>
+      <h1 className='text-4xl text-dark font-bold capitalize mb-10 lg:text-7xl'>Exprience</h1>
 
-      <div className='w-full mx-auto relative'>
+      <div className='w-full mx-auto relative space-y-5'>
         <ul>
             <Details 
             position="Front-End Intern" company="dammy.org"
+            companyLink="www.dammy.org"
+            year="2023 - present" address="CA"
+            workDetails="Worked on the UI with React & Tailwind"
+            />
+        </ul>
+        <ul>
+            <Details 
+            position="Front-End Intern" company="dammy.org"
+            companyLink="www.dammy.org"
+            year="2023 - present" address="CA"
+            workDetails="Worked on the UI with React & Tailwind"
+            />
+        </ul>
+        <ul>
+            <Details 
+            position="Front-End Intern" company="dammy.org"
+            companyLink="www.dammy.org"
+            year="2023 - present" address="CA"
+            workDetails="Worked on the UI with React & Tailwind"
+            />
+        </ul>
+        <ul>
+            <Details 
+            position="Front-End Intern" company="dammy.org"
+            companyLink="www.dammy.org"
             year="2023 - present" address="CA"
             workDetails="Worked on the UI with React & Tailwind"
             />
--- src/components/details.js
+++ src/components/details.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Details = ({ position, company, address, year, workDetails }) => {
+const Details = ({ position, company, companyLink, address, year, workDetails }) => {
   return ( <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
     <div>
-      <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; @{company}</h3>
+      <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a href={companyLink} target='_blank' className='capitalize text-blue-700'>@{company}</a></h3>
       <span className='capitalize font-medium text-dark/70'>
         {year} | {address}
       </span>
