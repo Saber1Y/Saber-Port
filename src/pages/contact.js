7d45fc28b9f4113c23d767858664653dd50f5332
ayodeji <ayodejiakintobi1@gmail.com>
2024-02-02 21:30:04 +0100

    UPDATE: CONTACT FILE ADDED

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const contact = () => {
+
+    const bgImage = "/public/images/profile/bg.png";
+
+    return (
+        <section className="bg-center bg-no-repeat bg-[url('')] bg-blend-multiply">
+            <h2>Got a Project ?</h2>
+            <p>
+                We help brands and platforms turn big ideas into beautiful digital
+                products and exprineces.
+            </p>
+
+
+        </section>
+    );
+};
+
+export default contact;
