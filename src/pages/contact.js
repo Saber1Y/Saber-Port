747455a0d94427f4a7472c7deba9b54856786c84
ayodeji <ayodejiakintobi1@gmail.com>
2024-02-05 17:15:01 +0100

    UPDATE: CONTACT UPDATED

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
                 <title>Saber | About Page</title>
                 <meta name="viewport" content="any"></meta>
             </Head>
-            <section className="bg-center bg-no-repeat bg-[url('')] bg-blend-multiply">
+            <section className="bg-center bg-no-repeat  bg-blend-multiply">
                 <h2>Got a Project ?</h2>
                 <p>
                     We help brands and platforms turn big ideas into beautiful digital
