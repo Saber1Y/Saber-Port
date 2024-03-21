768aed57b86a48b241552cfafa928fc3c5b028ae
ayodeji <ayodejiakintobi1@gmail.com>
2024-03-21 21:13:30 +0100

    UPDATE: COPY ICON

---
+++ src/components/Copy.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Copy = ({ icon, text }) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyText = () => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => setCopied(true))
+      .catch((error) => console.error("Error copying text: ", error));
+  };
+
+  return (
+    <div>
+      <button onClick={copyText}>{icon}</button>
+      {copied && <span>text Copied!</span>}
+    </div>
+  );
+};
+
+export default Copy;
